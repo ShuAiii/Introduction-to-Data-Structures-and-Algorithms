820bc9232c66c641640b813013c12a52d0e546ff
ShuAii <zhangs37@mcmaster.ca>
2020-08-12 18:15:40 -0400

    added hint1 for Assignemnet 1

---
+++ Week1/Hint1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 16:56:03 2020
+
+@author:  Teacher
+"""
+
+"Question 1"
+
+A= input ("Enter a number 1: ")
+AnswerA= float (A)
+
+B= input ("Enter a number 2: ")
+AnswerB= float (B)
+print("Sum is" , AnswerA + AnswerB)
+ 
+
+"Question 2"
+Name = input("What is your name: ")
+
+Sport = input("What is your favourite sport: ")
+
+print(Name + " my favourite sport is " + Sport + " too!")
+
+
+ 
